d4df237d90f17a937b76b0e35f50b882c83f1c85
mykhailo.merezhko <mykhailo.merezhko@techs.com.ua>
2021-03-21 18:05:24 +0200

    Read the docs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex">
+    <nav class="nav">
+      <img alt="Vue logo" src="./assets/logo.png" height="100" width="100" />
+    </nav>
+
+    <HelloWorld msg="GitHub" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +24,24 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #404660;
+  height: 100vh;
+  background: #ecf0f3;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.flex {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.nav {
+  margin: 12px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main class="soft flex">
+    <h1>Enter {{ msg }} user name</h1>
+    <section class="neumorphism flex">
+      <label class="label">{{ username }}</label>
+      <input type="text" v-model="username" placeholder="Enter..." />
+    </section>
+    <button class="btn" @click="option = username">Submit</button>
+  </main>
+
+  <ol class="soft" v-if="option">
+    <li v-for="lib in data" :key="lib.name">
+      {{ lib.name }} {{ new Date(lib.updated_at).toDateString() }}
+    </li>
+  </ol>
 </template>
 
 <script>
+import { reactive, ref, watchEffect, toRefs } from "vue";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  setup() {
+    const username = ref(null);
+    const option = ref(null);
+
+    const state = reactive({
+      data: [],
+    });
+
+    watchEffect(() => {
+      if (option.value) {
+        fetch(`https://api.github.com/users/${option.value}/repos`)
+          .then((response) => response.json())
+          .then((data) => {
+            state.data = data;
+          });
+      }
+    });
+
+    return {
+      username,
+      option,
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.soft {
+  padding: 16px 20px;
+  border-radius: 8px;
+  background: #ecf0f3;
+  box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
+  font-size: 16px;
+  margin-bottom: 12px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.neumorphism {
+  position: relative;
+  transition: transform 0.3s;
+  transform: scale(0.96, 1) translateZ(0);
+  margin: 16px 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.label {
+  text-transform: uppercase;
+  margin-bottom: 8px;
 }
-a {
-  color: #42b983;
+
+.btn {
+  background: #e3e6ec;
+  box-shadow: 18px 18px 20px #d1d9e6, -18px -18px 20px #ffffff;
+  border-radius: 30px;
+  padding: 16px 20px;
+  border: 0;
+  font-size: inherit;
+  font-weight: bold;
+}
+
+.btn:hover {
+  background: #e6e9ef;
+  box-shadow: inset 18px 18px 30px #d1d9e6, inset -18px -18px 30px #ffffff;
+}
+
+input {
+  padding: 10px 12px;
 }
 </style>
