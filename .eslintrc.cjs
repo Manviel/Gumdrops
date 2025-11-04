0648c76cf2e50929461973287f36a29d57c745e6
Manviel <mmere@softserveinc.com>
2025-11-04 15:10:10 +0200

    Migrate from vue-cli-service to vite

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {}
+}
