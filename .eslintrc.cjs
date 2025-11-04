634efb501a6a3dfda6481411a03e7131ebb9eae5
Manviel <mmere@softserveinc.com>
2025-11-04 15:12:44 +0200

    Drop babel deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,6 @@ module.exports = {
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
     ecmaVersion: 2020,
     sourceType: 'module',
   },
